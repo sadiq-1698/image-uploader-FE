9d512d7adeeae1052e9b24cc71dc777e0b777702
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-05 22:04:37 +0530

    setting up initial variables

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CenterCard from "./components/CenterCard/CenterCard";
+
+function App() {
+  return <ImageUploader />;
+}
+
+const ImageUploader = () => {
+  return <CenterCard />;
+};
+
+export default App;
---
+++ src/components/CenterCard/CenterCard.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const CenterCard = () => {
+  return (
+    <div className="center-card">
+      <p className="header">Upload your image</p>
+      <p className="file-type">File should be Jpeg, Png,...</p>
+    </div>
+  );
+};
+
+export default CenterCard;
