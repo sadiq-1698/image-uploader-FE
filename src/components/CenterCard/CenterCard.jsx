c54a01d5370efa049d5a9221a919e0007f24cf2d
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-03-07 04:26:07 +0530

    added global styles

--- src/components/CenterCard/CenterCard.jsx
+++ src/components/CenterCard/CenterCard.jsx
@@ -1,10 +1,13 @@
 import "./styles.css";
+import ImageDropzone from "../ImageDropzone/ImageDropzone";
 
 const CenterCard = () => {
   return (
     <div className="center-card">
       <p className="header">Upload your image</p>
-      <p className="file-type">File should be Jpeg, Png,...</p>
+      <p className="instruction">File should be Jpeg, Png,...</p>
+      <ImageDropzone />
+      <p className="instruction">or</p>
     </div>
   );
 };
---
+++ src/components/ImageDropzone/ImageDropzone.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const ImageDropzone = () => {
+  return (
+    <div className="image-dropzone">
+      <h1>Image Dropzone</h1>
+    </div>
+  );
+};
+
+export default ImageDropzone;
